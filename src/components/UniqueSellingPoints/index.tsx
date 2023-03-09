00f50272118680b3374061f80c47ffd969439c57
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-03-09 12:41:46 +0000

    Style Unique Selling Points

--- src/components/UniqueSellingPoints/index.tsx
+++ src/components/UniqueSellingPoints/index.tsx
@@ -1,4 +1,5 @@
 import { LinkType } from "@/graphql/generated/graphql"
+import { Card, Container, Flex, Grid, Heading, Link, ListItem, SimpleGrid, Stack, Text, UnorderedList, VStack } from "@chakra-ui/react"
 import Image from "next/image"
 
 interface IUniqueSellingPointsProps {
@@ -20,34 +21,51 @@ interface IUniqueSellingPointsProps {
 
 export function UniqueSellingPoints({ title, sellingPoints }: IUniqueSellingPointsProps) {
   return (
-    <section>
-      <h4>{title}</h4>
-      {sellingPoints?.length > 0 && (
-        <ul>
-          {sellingPoints.map(point => (
-            <li key={point.title}>
-              <p>{point.title}</p>
-              <p>{point.text}</p>
+    <Container maxW='5xl'>
+      <Stack
+        py={{ base: 10, md: 16 }}
+        spacing={{ base: 5, md: 10 }}
+      >
+        <Heading
+          textAlign='center'
+          fontSize={{ base: 'xl', sm: '2xl', md: '3xl' }}
+        >
+          {title}
+        </Heading>
 
-              {point.link && point.link.length > 0 && (
-                <ul>
-                  {point.link.map(item => (
-                    <li key={item.url}>
-                      <a href={item.url} target={item.target ?? ''}>
-                        {item.name} - {item.type}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              )}
+        {sellingPoints?.length > 0 && (
+          <SimpleGrid minChildWidth='300px' spacing={6}>
+            {sellingPoints.map(point => (
+              <Card key={point.title} p={5}>
+                <Flex flexDirection='column' gap={1}>
+                  {point.image && (
+                    <Image src={point.image.url} width={200} height={200} alt="" />
+                  )}
 
-              {point.image && (
-                <Image src={point.image.url} width={200} height={200} alt="" />
-              )}
-            </li>
-          ))}
-        </ul>
-      )}
-    </section>
+                  <Heading size='md'>{point.title}</Heading>
+                  <Text
+                    mt={2}
+                    fontSize='sm'
+                    color='gray.600'
+                  >{point.text}</Text>
+
+                  {point.link && point.link.length > 0 && (
+                    <UnorderedList mt={2}>
+                      {point.link.map(item => (
+                        <ListItem key={item.url}>
+                          <Link href={item.url} target={item.target ?? ''}>
+                            {item.name} - {item.type}
+                          </Link>
+                        </ListItem>
+                      ))}
+                    </UnorderedList>
+                  )}
+                </Flex>
+              </Card>
+            ))}
+          </SimpleGrid>
+        )}
+      </Stack>
+    </Container >
   )
 }
